80e2e7585b16b3a6ba69437e95762af5c6470f22
Sebastien Marichal <sebmarichal@gmail.com>
2020-08-30 11:40:37 +0200

    Database configuration

--- OpenWT.Contact.Data/Interop/ContactDataServiceConfiguration.cs
+++ OpenWT.Contact.Data/Interop/ContactDataServiceConfiguration.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OpenWT.Contact.Data.Context;
 
 namespace OpenWT.Contact.Data.Interop
 {
@@ -7,6 +9,20 @@ namespace OpenWT.Contact.Data.Interop
     {
         public static void AddContactDataServices(this IServiceCollection services, IConfiguration configuration)
         {
+            var connectingString = configuration.GetValue<string>("DbSettings:ConnectionStrings");
+
+            switch (configuration.GetValue<string>("DbSettings:SqlProvider")?.ToLower())
+            {
+                case "postgre":
+                case "postgresql":
+                    services.AddDbContext<ContactContext>(opt => opt.UseNpgsql(connectingString));
+                    break;
+                case "sqlite":
+                    break;
+                case "sqlserver":
+                default:
+                    break;
+            }
         }
     }
 }
