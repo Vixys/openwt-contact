489a574426af31a44288e928b17fb4eeafc19414
Sebastien Marichal <sebmarichal@gmail.com>
2020-08-30 12:15:38 +0200

    Rename project OpenWT.Contact.Service to OpenWT.Contact.Application

--- OpenWT.Contact.Application/Interop/ContactApplicationServiceConfiguration.cs
+++ OpenWT.Contact.Application/Interop/ContactApplicationServiceConfiguration.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OpenWT.Contact.Application.Contract;
+using OpenWT.Contact.Application.Service;
 using OpenWT.Contact.Infrastructure.Interop;
 
-namespace OpenWT.Contact.Service.Interop
+namespace OpenWT.Contact.Application.Interop
 {
     public static class ContactApplicationServiceConfiguration
     {
         public static void AddContactApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddScoped<IContactService, ContactService>();
+            services.AddScoped<ISkillService, SkillService>();
+            
             services.AddContactInfrastructureServices(configuration);
         }
     }
